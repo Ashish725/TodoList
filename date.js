4fffaf65afcb038997dc647c7a5a7a5d95dc2936
Ashish725 <ashishmandal7766@gmail.com>
2022-05-14 08:57:01 +0530

    initial commit

---
+++ date.js
@@ -0,0 +1,12 @@
+exports.getDate= function()
+{
+  let today= new Date();
+
+  let option ={
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  };
+  let day= today.toLocaleDateString("en-US",option);
+  return day;
+};
