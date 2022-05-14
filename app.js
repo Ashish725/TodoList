3f8eb0b46ddb121001c00c20f695904d67fe47d8
Ashish725 <ashishmandal7766@gmail.com>
2022-05-14 09:38:18 +0530

    update app.js

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.get("/work", function(req,res){
 app.get("/about",function(req,res){
   res.render("about");
 });
-app.listen(process.env.PORT,function(){
+app.listen(process.env.PORT || 3000,function(){
   console.log("server is started on port 3000");
 });
